874992abc45728463ef8fc1a3eda002cd396a97a
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-28 21:41:51 +0200

    eerste commit

--- src/Opdracht4_1/GelijkbenigeDriehoek.java
+++ src/Opdracht4_1/GelijkbenigeDriehoek.java
@@ -1,4 +1,16 @@
 package Opdracht4_1;
 
-public class GelijkbenigeDriehoek {
+import java.awt.*;
+import java.applet.*;
+
+public class GelijkbenigeDriehoek extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50, 200, 100, 110);
+        g.drawLine(100, 110, 150, 200);
+        g.drawLine(50, 200, 150, 200);
+    }
 }
