88b0685941bd6496d456786fcfce7afe86a98dbd
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-28 23:33:51 +0200

    tweede commit

--- src/Opdracht4_3/Vlag.java
+++ src/Opdracht4_3/Vlag.java
@@ -1,4 +1,23 @@
 package Opdracht4_3;
 
-public class Vlag {
+import java.awt.*;
+import java.applet.*;
+
+public class Vlag extends Applet {
+
+      public void init() {
+      }
+
+      public void paint(Graphics g) {
+          g.setColor(Color.red);
+          g.fillRect(40, 40, 200, 45);
+          g.setColor(Color.white);
+          g.fillRect(40, 80, 200, 45);
+          g.setColor(Color.blue);
+          g.fillRect(40, 120,200, 45);
+          g.setColor(Color.gray);
+          g.fillRect(20, 30 , 20, 300);
+      }
+
+
 }
