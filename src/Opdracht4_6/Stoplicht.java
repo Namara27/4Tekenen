ead51f99d92fe29eca2c264bf60c37e552667db6
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-29 22:32:29 +0200

    tweede commit

--- src/Opdracht4_6/Stoplicht.java
+++ src/Opdracht4_6/Stoplicht.java
@@ -1,4 +1,21 @@
 package Opdracht4_6;
 
-public class Stoplicht {
+import java.awt.*;
+import java.applet.*;
+
+public class Stoplicht extends Applet {
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+            g.fillRect(112,140,17,110);
+            g.fillRoundRect(100,50,40,100,10,10);
+            g.setColor(Color.red);
+            g.fillArc(110,60,20,20,0,360);
+            g.setColor(Color.orange);
+            g.fillArc(110,90,20,20,0,360);
+            g.setColor(Color.green);
+            g.fillArc(110,120,20,20,0,360);
+        }
 }
