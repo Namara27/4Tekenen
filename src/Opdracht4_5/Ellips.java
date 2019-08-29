6ec2e55a4cf6243113daafdf25866e028ae08e23
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-29 21:59:12 +0200

    tweede commit

--- src/Opdracht4_5/Ellips.java
+++ src/Opdracht4_5/Ellips.java
@@ -1,4 +1,16 @@
 package Opdracht4_5;
 
-public class Ellips {
+import java.awt.*;
+import java.applet.*;
+
+public class Ellips extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillArc(20, 20, 100, 100, 0, 360);
+    }
 }
