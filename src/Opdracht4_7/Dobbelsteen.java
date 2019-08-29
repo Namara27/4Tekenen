ad0940c8bc26793a9f44f958f6f5846778a7d229
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-29 22:45:53 +0200

    tweede commit

--- src/Opdracht4_7/Dobbelsteen.java
+++ src/Opdracht4_7/Dobbelsteen.java
@@ -1,4 +1,19 @@
 package Opdracht4_7;
 
-public class Dobbelsteen {
+import java.awt.*;
+import java.applet.*;
+
+public class Dobbelsteen extends Applet {
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+            g.fillRoundRect(100,100,100,100, 20,20);
+            g.setColor(Color.white);
+            g.fillArc(115,115,20,20,0,360);
+            g.fillArc(115,165,20,20,0,360);
+            g.fillArc(165,115,20,20,0,360);
+            g.fillArc(165,165,20,20,0,360);
+        }
 }
